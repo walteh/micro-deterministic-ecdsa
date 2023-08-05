769050eb569759c7c61a4b8381cfb4acf3299a15
walter <iamwalteh@me.com>
2023-08-05 13:28:06 -0500

    pre cleanup

--- Package.swift
+++ Package.swift
@@ -16,8 +16,19 @@ let package = Package(
 	],
 	dependencies: [],
 	targets: [
-		.target(name: "CRFC6979"),
-		.target(name: "RFC6979", dependencies: ["CRFC6979"]),
-		.testTarget(name: "RFC6979Tests", dependencies: ["RFC6979"]),
+		.target(
+			name: "CRFC6979",
+			cSettings: [.unsafeFlags(["-fprofile-instr-generate", "-fcoverage-mapping"])]
+		),
+		.target(
+			name: "RFC6979",
+			dependencies: ["CRFC6979"],
+			cSettings: [.unsafeFlags(["-fprofile-instr-generate", "-fcoverage-mapping"])]
+		),
+		.testTarget(
+			name: "RFC6979Tests",
+			dependencies: ["RFC6979", "CRFC6979"],
+			cSettings: [.unsafeFlags(["-fprofile-instr-generate", "-fcoverage-mapping"])]
+		),
 	]
 )
