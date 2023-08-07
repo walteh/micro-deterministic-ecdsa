2d3c8b20d8669b0572d5a883b865d43d2d118817
walter <iamwalteh@me.com>
2023-08-07 06:18:59 -0500

    cleanup

--- Package.swift
+++ Package.swift
@@ -18,17 +18,14 @@ let package = Package(
 	targets: [
 		.target(
 			name: "CRFC6979"
-			// cSettings: [.unsafeFlags(["-fprofile-instr-generate", "-fcoverage-mapping"])]
 		),
 		.target(
 			name: "RFC6979",
 			dependencies: ["CRFC6979"]
-			// cSettings: [.unsafeFlags(["-fprofile-instr-generate", "-fcoverage-mapping"])]
 		),
 		.testTarget(
 			name: "RFC6979Tests",
 			dependencies: ["RFC6979", "CRFC6979"]
-			// cSettings: [.unsafeFlags(["-fprofile-instr-generate", "-fcoverage-mapping"])]
 		),
 	]
 )
